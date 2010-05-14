f25f6bcaf70e81c0263b55040c2e2460efd59f33
Hazim Gazov <hazim.gazov@gmail.com>
2010-05-14 17:53:17 -0300

    importing everything from the imgdb svn repo

---
+++ .svn/text-base/ImgdbEngine.java.svn-base
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package net.imgseek.imgdb.core;
+
+/**
+ * @author rnc
+ *
+ */
+public class ImgdbEngine extends imgdb {
+	static {
+		System.loadLibrary("imgdb");
+	}
+
+	public ImgdbEngine() {
+		super();
+	}
+}
---
+++ gen_swig.bat
@@ -0,0 +1,4 @@
+cd I:\workspaces\workspace-callisto\net.imgseek.imgdb\src\net\imgseek\imgdb\core
+i:\prj\swigwin-1.3.31\swig -Wall -c++ -java -package net.imgseek.imgdb.core -o imgdb_wrap.h imgdb.i
+i:\prj\swigwin-1.3.31\swig -Wall -c++ -python -o imgdb_python_wrap.h imgdb.i
+pause
